55417d6d94bb29326f8b0faf1252ac3dda3586aa
ola <olacencora@gmail.com>
2019-08-25 13:56:45 +0200

    ready ap

---
+++ src/components/menu.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import {Link} from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default function SimpleMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+      <MenuIcon />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {/* uzywwamy komponent linkt pod jaka sciezke */}
+        <MenuItem onClick={handleClose}><Link to={'/'}>Home</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to={'/about'}>About</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to={'/beers'}>Beers List</Link></MenuItem>
+      </Menu>
+    </div>
+  );
+}
